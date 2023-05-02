066a5d1629580c7efcb5a789690b9a4f8657e35a
yash_palkar <111225341+yashpalkar2001@users.noreply.github.com>
2023-05-02 14:58:07 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Navbar from './components/navbar/navbar'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+<Navbar />
+ 
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './navbar.css'
+
+
+ 
+function navbar(){
+return(
+    <>
+<div className='myNav'>
+    
+        <li>Home</li>
+        <li>Groceries</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+     
+</div>
+    </>
+)
+}
+export default navbar;
